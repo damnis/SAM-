2a6b3bd847979511c90cb0ff407a82207b17e08d
damnis <36800988+damnis@users.noreply.github.com>
2025-06-23 15:21:18 +0200

    Update Sam_app.py

--- Sam_app.py
+++ Sam_app.py
@@ -1,6 +1,9 @@
 sam_app.py
 
-import streamlit as st import pandas as pd import numpy as np import matplotlib.pyplot as plt
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
 
 Simuleer koersdata
 
